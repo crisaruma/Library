f519f5707a5ad12373b16dcaf793df1c76b8ec83
t_takesada <t.takesada@exzeal.co.jp>
2017-12-28 19:36:11 +0900

    ハッシュクラス追加

---
+++ proj/TestLib/TestLib/include/LibHash.h
@@ -0,0 +1,25 @@
+﻿
+#ifndef __LIB_HASH_H__
+#define __LIB_HASH_H__
+
+#include "LibType.h"
+
+
+namespace CLib
+{
+    class CHash
+    {
+        public:
+            static const Uint32 CRC32(const char* _pVal );
+            static const Uint32 CRC32(const char* _pVal , const Uint32& _size );
+
+        public:
+            static const Uint32 mMask;
+            static const Uint32 mTable[256];
+    };
+
+};
+
+
+
+#endif
